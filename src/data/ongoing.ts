ea4d6fb0926c209f802ec70b3a745014a786cb51
Byeonghyun Pak <byeonghyun.pak@gmail.com>
2025-10-21 23:15:49 +0900

    hide dynamics-aware policy learning

--- src/data/ongoing.ts
+++ src/data/ongoing.ts
@@ -7,12 +7,12 @@ export interface Ongoing {
 }
 
 export const OngoingData: Ongoing[] = [
-  {
-    title: "Dynamics-Aware Policy Learning",
-    state: "Active",
-    tldr: "We learn policies that model the underlying dynamics of an environment to forecast short-horizon. This look-ahead enables anticipatory behavior such as intercepting moving targets.",
-    imageUrl: "/images/mvtracker.gif",
-  },
+  // {
+  //   title: "Dynamics-Aware Policy Learning",
+  //   state: "Active",
+  //   tldr: "We learn policies that model the underlying dynamics of an environment to forecast short-horizon. This look-ahead enables anticipatory behavior such as intercepting moving targets.",
+  //   imageUrl: "/images/mvtracker.gif",
+  // },
   {
     title: "Title Anonymized for Double-Blind Review",
     state: "Submitted to ICLR 2026",
